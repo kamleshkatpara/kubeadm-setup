d546ac556e81efdd8ce4b009c9e31a7d68470795
Kamlesh Katpara <16983847+kamleshkatpara@users.noreply.github.com>
2023-11-26 04:29:46 +0530

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -3,11 +3,13 @@
 # Exit on error, undefined variable, or error in any pipeline
 set -euxo pipefail
 
+# Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root."
     exit 1
 fi
 
+# Function to log messages with timestamp
 log() {
     echo "$(date +"%Y-%m-%d %H:%M:%S") - $1"
 }
@@ -18,38 +20,46 @@ log "Starting script..."
 KUBERNETES_VERSION="1.28.2-00"
 OS="xUbuntu_22.04"
 VERSION="1.28"
-PRIMARY_INTERFACE="eth1"
+PRIMARY_INTERFACE="eth0"
 
-# Disable swap and keep it off after reboot
+# Disable swap and prevent it from starting after reboot
 sudo swapoff -a
 sudo sed -i 's/^\/swap\.img/#\/swap.img/' /etc/fstab
 
 # Update package list
 sudo apt-get update -y
 
+# Load kernel modules
 sudo bash -c 'cat <<EOF > /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF'
 
+# Load kernel modules immediately
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-# sysctl params required by setup, params persist across reboots
+# Configure sysctl parameters for Kubernetes setup
 sudo bash -c 'cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF'
 
-# Apply sysctl params without reboot
+# Apply sysctl parameters without reboot
 sudo sysctl --system
 
+# Print current sysctl parameters
 sudo sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
 
-# Add CRI-O to sources and install
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+# Add CRI-O repository and install
+sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list <<EOF
+deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+EOF
+
+sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list <<EOF
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+EOF
 
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
@@ -57,14 +67,13 @@ curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/
 sudo apt-get update
 sudo apt-get install -y cri-o cri-o-runc
 
+# Enable and start CRI-O service
 sudo systemctl daemon-reload
 sudo systemctl enable crio --now
 
-echo "CRI runtime installed successfully"
+log "CRI runtime installed successfully"
 
 # Install Kubernetes components
-sudo apt-get update -y
-
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
@@ -73,8 +82,9 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 
 sudo apt-get update -y
 sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
-sudo apt-mark hold kubelet kubeadm kubectl
 
+# Prevent Kubernetes packages from being automatically upgraded
+sudo apt-mark hold kubelet kubeadm kubectl
 
 # Install jq and set Kubelet node IP
 sudo apt-get install -y jq
@@ -83,4 +93,4 @@ local_ip=$(ip --json addr show $PRIMARY_INTERFACE | jq -r '.[0].addr_info[] | se
 
 echo "KUBELET_EXTRA_ARGS=--node-ip=$local_ip" | sudo tee /etc/default/kubelet > /dev/null
 
-echo "Kubeadm, Kubelet, Kubectl installed successfully !"
+log "Kubeadm, Kubelet, Kubectl installed successfully!"
